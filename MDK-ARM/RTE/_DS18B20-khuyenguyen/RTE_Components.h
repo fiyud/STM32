429fc1b7b1e511673e6481092721e072fedbf284
fiyud <chotala1113@gmail.com>
2024-12-04 11:18:15 +0700

    .

---
+++ MDK-ARM/RTE/_DS18B20-khuyenguyen/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'DS18B20-khuyenguyen' 
+ * Target:  'DS18B20-khuyenguyen' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
